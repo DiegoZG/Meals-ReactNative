6bd674499bf5918dd0b4b8268c0dba5214e209ae
Diwo777 <diego.zegarra79@icloud.com>
2021-02-17 19:50:39 -0500

    added views

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Text> Search</Text>
+      </View>
+      <View style={styles.meals}>
+        <Text> Meals</Text>
+      </View>
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'stretch'
   },
+  search: {
+    height: 50,
+    backgroundColor: 'green',
+    justifyContent: 'center'
+  },
+  meals: {
+    flex: 1,
+    backgroundColor: 'blue',
+    paddingTop: 20
+    
+  }
 });
