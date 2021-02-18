c563597fe13b5a97cc8669e2c78bf0c77239d25f
Diwo777 <diego.zegarra79@icloud.com>
2021-02-18 11:39:18 -0500

    added react native paper

--- App.js
+++ App.js
@@ -1,43 +1,45 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
-console.log(StatusBar.currentHeight)
 // const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <> 
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Text> Search</Text>
-      </View>
-      <View style={styles.meals}>
-        <Text> Meals</Text>
-      </View>
-    </SafeAreaView>
-    <ExpoStatusBar style="auto"/>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar />
+        </View>
+        <View style={styles.list}>
+          <Text> Meals</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
     marginTop: StatusBar.currentHeight,
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'stretch'
   },
   search: {
-    height: 50,
-    backgroundColor: 'green',
-    justifyContent: 'center'
+    padding: 16,
+    backgroundColor: "green",
   },
-  meals: {
+  list: {
     flex: 1,
-    backgroundColor: 'blue',
-    paddingTop: 20
-    
-  }
+    backgroundColor: "blue",
+    padding: 16,
+  },
 });
