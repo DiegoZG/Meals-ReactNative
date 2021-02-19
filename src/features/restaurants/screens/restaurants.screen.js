8deb3783c3e187f9ab60d7d0c8c3d11ea0dabc2d
Diwo777 <diego.zegarra79@icloud.com>
2021-02-19 15:46:09 -0500

    Fixed StatusBar bug

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,33 +1,34 @@
 import React from "react";
 import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import styled from "styled-components/native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { Searchbar } from "react-native-paper";
+import { List, Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const ListContainer = styled.View`
+  flex: 1;
+  background-color: blue;
+  padding: 16px;
+`;
+
 export const RestaurantsScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
+    <SafeArea>
+      <SearchContainer>
         <Searchbar />
-      </View>
-      <View style={styles.list}>
+      </SearchContainer>
+      <ListContainer>
         <RestaurantInfoCard />
-      </View>
-    </SafeAreaView>
+      </ListContainer>
+    </SafeArea>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: StatusBar.currentHeight,
-    flex: 1,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  list: {
-    flex: 1,
-    backgroundColor: "blue",
-    padding: 16,
-  },
-});
