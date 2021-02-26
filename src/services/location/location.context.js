43f8d114fbbf3c7bed1a53b365737d6fcfae083c
Diwo777 <diego.zegarra79@icloud.com>
2021-02-26 17:38:43 -0500

    Added useState in location context

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -1,1 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
+
+import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = createContext();
+
+export const LocationContext = ({ children }) => {
+  const [keyword, setKeyword] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  return (
+    <LocationContext.Provider
+      value={{
+        isLoading,
+        error,
+        location,
+        search: () => null,
+        keyword,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
