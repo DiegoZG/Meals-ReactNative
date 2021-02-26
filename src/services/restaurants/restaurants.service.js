ff591a72e30743fe9fe1b32ad85b7d3e7262ad7c
Diwo777 <diego.zegarra79@icloud.com>
2021-02-26 11:35:15 -0500

    Added key value pairs to results object

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -11,9 +11,22 @@ export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 
+const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+
+  return camelize(mappedResults);
+};
+
 restaurantRequest()
-  .then((result) => {
-    console.log(result);
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
   })
   .catch((err) => {
     console.log("error");
