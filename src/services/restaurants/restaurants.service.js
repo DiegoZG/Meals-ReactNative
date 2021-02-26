8f3fd6ae9cdca663d550e9a7f90d3fae45374c20
Diwo777 <diego.zegarra79@icloud.com>
2021-02-26 10:58:31 -0500

    Added camelize

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
 
 export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
