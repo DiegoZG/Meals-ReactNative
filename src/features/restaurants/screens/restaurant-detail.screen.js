e62f72e57f7b8959be4b2a8094317a2e71fdf9a4
Diwo777 <diego.zegarra79@icloud.com>
2021-03-01 11:06:33 -0500

    Started restaurant detail screen

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { List } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const RestaurantDetailScreen = ({ route }) => {
   const { restaurant } = route.params;
+  const [breakfastExpanded, setBreakfastExpanded] = useState(false);
+  const [dinnerExpanded, setDinnerExpanded] = useState(false);
+  const [lunchExpanded, setLunchExpanded] = useState(false);
+  const [drinksExpanded, setDrinksExpanded] = useState(false);
   return (
     <SafeArea>
       <RestaurantInfoCard restaurant={restaurant} />
