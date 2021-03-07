b611d792891a62820c754a7d5fc06df430ca4d7c
Diwo777 <diego.zegarra79@icloud.com>
2021-03-06 19:03:15 -0500

    Imported styles to login screen

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { AccountBackground } from "../components/account.styles";
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 export const LoginScreen = () => {
   return <AccountBackground />;
